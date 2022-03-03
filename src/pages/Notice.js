a0c622e79273dd9f63eaca71dbf63b149dfd95e6
serenaa2 <2dltnwls@gmail.com>
2022-03-03 15:43:53 +0900

    1.고객센터 테이블,글쓰기 추가 2.공지사항 테이블 추가

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -1,52 +1,133 @@
-import React from "react";
-import "./Checkout.css";
+import "antd/dist/antd.css";
+import { Button, Table, Modal, Input } from "antd";
+import { useState } from "react";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
- import { Table,Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
- import { Route, Link } from "react-router-dom";
-import Write from "./Write";
+function App() {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingStudent, setEditingStudent] = useState(null);
+    const [dataSource, setDataSource] = useState([
+        {
+            id: 1,
+            notice: "금일 점검 시간 안내(오후 3시~4시)",
+        },
+        {
+            id: 2,
+            notice: "배송 지연 사항 안내",
+        },
+        {
+            id: 3,
+            notice: "오류 해결방법 안내",
+        },
+        {
+            id: 4,
+            notice: "공지사항",
+        },
+    ]);
+    const columns = [
+        {
+            key: "1",
+            title: "ID",
+            dataIndex: "id",
+        },
+        {
+            key: "2",
+            title: "notice",
+            dataIndex: "notice",
+        },
+        {
+            key: "5",
+            title: "Actions",
+            render: (record) => {
+                return (
+                    <>
+                        <EditOutlined
+                            onClick={() => {
+                                onEditStudent(record);
+                            }}
+                        />
+                        <DeleteOutlined
+                            onClick={() => {
+                                onDeleteStudent(record);
+                            }}
+                            style={{ color: "red", marginLeft: 12 }}
+                        />
+                    </>
+                );
+            },
+        },
+    ];
 
-
-function Notice() {
-
-
-  return (
-      <>
-      <h1>공지사항 입니다.</h1>
-
-      <Table striped bordered hover>
-          <thead>
-          <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-          </tr>
-          <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-          </tr>
-          <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-          </tr>
-          </tbody>
-      </Table>
-
-
-      </>
-  );
+    const onAddStudent = () => {
+        const randomNumber = parseInt(Math.random() * 1000);
+        const newStudent = {
+            id: randomNumber,
+            name: "Name " + randomNumber,
+            email: randomNumber + "@gmail.com",
+            notice: "notice " + randomNumber,
+        };
+        setDataSource((pre) => {
+            return [...pre, newStudent];
+        });
+    };
+    const onDeleteStudent = (record) => {
+        Modal.confirm({
+            title: "지우시겠습니까?",
+            okText: "네",
+            okType: "아니요",
+            onOk: () => {
+                setDataSource((pre) => {
+                    return pre.filter((student) => student.id !== record.id);
+                });
+            },
+        });
+    };
+    const onEditStudent = (record) => {
+        setIsEditing(true);
+        setEditingStudent({ ...record });
+    };
+    const resetEditing = () => {
+        setIsEditing(false);
+        setEditingStudent(null);
+    };
+    return (
+        <div className="App">
+            <h1>공지사항</h1>
+            <header className="App-header">
+                <Button onClick={onAddStudent}>글 추가</Button>
+                <Table columns={columns} dataSource={dataSource}></Table>
+                <Modal
+                    title="Edit Student"
+                    visible={isEditing}
+                    okText="Save"
+                    onCancel={() => {
+                        resetEditing();
+                    }}
+                    onOk={() => {
+                        setDataSource((pre) => {
+                            return pre.map((student) => {
+                                if (student.id === editingStudent.id) {
+                                    return editingStudent;
+                                } else {
+                                    return student;
+                                }
+                            });
+                        });
+                        resetEditing();
+                    }}
+                >
+                    <Input
+                        value={editingStudent?.notice}
+                        onChange={(e) => {
+                            setEditingStudent((pre) => {
+                                return { ...pre, notice: e.target.value };
+                            });
+                        }}
+                    />
+                </Modal>
+            </header>
+        </div>
+    );
 }
 
-export default Notice;
+export default App;
--- src/pages/Tables.js
+++ src/pages/Tables.js
@@ -1,54 +1,167 @@
-import React from "react";
-import "./Checkout.css";
-
- import { Table,Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
- import { Route, Link } from "react-router-dom";
-import Write from "./Write";
-
+import "antd/dist/antd.css";
+import { Button, Table, Modal, Input } from "antd";
+import { useState } from "react";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 function Tables() {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingStudent, setEditingStudent] = useState(null);
+    const [dataSource, setDataSource] = useState([
+        {
+            id: 1,
+            name: "John",
+            email: "john@gmail.com",
+            error: "창이 안떠요 ㅠㅠ",
+        },
+        {
+            id: 2,
+            name: "David",
+            email: "david@gmail.com",
+            error: "렉이 걸려요 ㅠㅠ",
+        },
+        {
+            id: 3,
+            name: "James",
+            email: "james@gmail.com",
+            error: "배송언제오죠??",
+        },
+        {
+            id: 4,
+            name: "Sam",
+            email: "sam@gmail.com",
+            error: "그냥올려봄",
+        },
+    ]);
+    const columns = [
+        {
+            key: "1",
+            title: "ID",
+            dataIndex: "id",
+        },
+        {
+            key: "2",
+            title: "Name",
+            dataIndex: "name",
+        },
+        {
+            key: "3",
+            title: "Email",
+            dataIndex: "email",
+        },
+        {
+            key: "4",
+            title: "error",
+            dataIndex: "error",
+        },
+        {
+            key: "5",
+            title: "Actions",
+            render: (record) => {
+                return (
+                    <>
+                        <EditOutlined
+                            onClick={() => {
+                                onEditStudent(record);
+                            }}
+                        />
+                        <DeleteOutlined
+                            onClick={() => {
+                                onDeleteStudent(record);
+                            }}
+                            style={{ color: "red", marginLeft: 12 }}
+                        />
+                    </>
+                );
+            },
+        },
+    ];
 
-
-  return (
-      <>
-      <h1>고객센터 입니다.</h1>
-
-      <Table striped bordered hover>
-          <thead>
-          <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-          </tr>
-          <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-          </tr>
-          <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-          </tr>
-          </tbody>
-      </Table>
-          <Link to="/Write" className="">
-              <button onClick="/Write">글쓰기</button>
-          </Link>
-
-      </>
-  );
+    const onAddStudent = () => {
+        const randomNumber = parseInt(Math.random() * 1000);
+        const newStudent = {
+            id: randomNumber,
+            name: "Name " + randomNumber,
+            email: randomNumber + "@gmail.com",
+            error: "error " + randomNumber,
+        };
+        setDataSource((pre) => {
+            return [...pre, newStudent];
+        });
+    };
+    const onDeleteStudent = (record) => {
+        Modal.confirm({
+            title: "지우시겠습니까?",
+            okText: "네",
+            okType: "아니요",
+            onOk: () => {
+                setDataSource((pre) => {
+                    return pre.filter((student) => student.id !== record.id);
+                });
+            },
+        });
+    };
+    const onEditStudent = (record) => {
+        setIsEditing(true);
+        setEditingStudent({ ...record });
+    };
+    const resetEditing = () => {
+        setIsEditing(false);
+        setEditingStudent(null);
+    };
+    return (
+        <div className="App">
+            <h1>고객센터</h1>
+            <header className="App-header">
+                <Button onClick={onAddStudent}>글 추가</Button>
+                <Table columns={columns} dataSource={dataSource}></Table>
+                <Modal
+                    title="Edit Student"
+                    visible={isEditing}
+                    okText="Save"
+                    onCancel={() => {
+                        resetEditing();
+                    }}
+                    onOk={() => {
+                        setDataSource((pre) => {
+                            return pre.map((student) => {
+                                if (student.id === editingStudent.id) {
+                                    return editingStudent;
+                                } else {
+                                    return student;
+                                }
+                            });
+                        });
+                        resetEditing();
+                    }}
+                >
+                    <Input
+                        value={editingStudent?.name}
+                        onChange={(e) => {
+                            setEditingStudent((pre) => {
+                                return { ...pre, name: e.target.value };
+                            });
+                        }}
+                    />
+                    <Input
+                        value={editingStudent?.email}
+                        onChange={(e) => {
+                            setEditingStudent((pre) => {
+                                return { ...pre, email: e.target.value };
+                            });
+                        }}
+                    />
+                    <Input
+                        value={editingStudent?.error}
+                        onChange={(e) => {
+                            setEditingStudent((pre) => {
+                                return { ...pre, error: e.target.value };
+                            });
+                        }}
+                    />
+                </Modal>
+            </header>
+        </div>
+    );
 }
 
 export default Tables;
