1bd3d0562c7e9aa8684f8da5cb6c39dafea555a2
eyhadoey <eyhad52@gmail.com>
2022-03-31 18:00:34 +0900

    220331 공지사항 세부페이지

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -1,6 +1,6 @@
 import "antd/dist/antd.css";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import Footer from "../components/Footer";
 
@@ -13,147 +13,38 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import Button from "@mui/material/Button";
+import axios from "axios";
 
 import { Link } from "react-router-dom";
 
 import "./Notice.css";
 
-const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-
-  { id: "Subname", label: "Subname", minWidth: 170 },
-  {
-    id: "Content",
-    label: "Content",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("ko-KR"),
-  },
-
-  {
-    id: "RegDate",
-    label: "RegDate",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("ko-KR"),
-  },
-];
-
-function createData(name, Subname, Content, RegDate, location) {
-  return { name, Subname, Content, RegDate, location };
-}
-
-const rows = [
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-  createData(
-    "운영자",
-    "15일 배송건에 대해서 공지사항 드립니다.",
-    "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
-
-    "2022-03-15"
-  ),
-];
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function ColumnGroupingTable() {
+  const [notice, setNotice] = useState([]);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const location = useLocation();
+  const id_data = location.state;
+  const [comment, setComment] = useState([]);
+
+  useEffect(() => {
+    getNotice();
+  }, []);
+
+  const getNotice = async () => {
+    await axios
+      .get("http://192.168.0.119:8080/MainView/noticeList")
+      .then((res) => {
+        console.log("가져온값 : " + res.data.length);
+        console.log("가져온값 : " + JSON.stringify(res.data));
+        setNotice(res.data);
+      })
+      .catch((error) => {
+        console.log(`getHome 에러 :  ${error.message}`);
+      });
+  };
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -183,49 +74,47 @@ export default function ColumnGroupingTable() {
               <TableRow>
                 {columns.map((column) => (
                   <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ top: 57, minWidth: column.minWidth }}
-                  >
-                    {column.label}
-                  </TableCell>
+                      key={column.id}
+                      align={column.align}
+                      style={{ top: 57, minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
                 ))}
               </TableRow>
             </TableHead>
 
             <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row.code}
-                    >
-                      {columns.map((column) => {
-                        const value = row[column.id];
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            <Link to="Posttwo" className="TableBody">
-                              {column.format && typeof value === "number"
-                                ? column.format(value)
-                                : value}
-                            </Link>
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
+              {notice.map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      console.log("너는 누구냐? : " + row.back_Notice_Idx);
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          <Link to={{
+                            pathname: "/Posttwo",
+                            id_value: row.back_Notice_Idx,
+                           }}
+                            className="TableBody">
+                            {column.format && typeof value === "number"
+                              ? column.format(value)
+                              : value}
+                          </Link>
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
             </TableBody>
           </Table>
         </TableContainer>
         <TablePagination
           rowsPerPageOptions={[10, 25, 100]}
           component="div"
-          count={rows.length}
+          count={notice.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
@@ -239,3 +128,128 @@ export default function ColumnGroupingTable() {
     </div>
   );
 }
+
+const columns = [
+  { id: "back_Notice_Idx", label: "Id", minWidth: 170 },
+
+  { id: "back_Notice_Subject", label: "Name", minWidth: 170 },
+  {
+    id: "back_Notice_Content",
+    label: "Content",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("ko-KR"),
+  },
+
+  {
+    id: "back_Notice_RegDate",
+    label: "RegDate",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("ko-KR"),
+  },
+];
+
+// function createData(name, Subname, Content, RegDate, location) {
+//   return { name, Subname, Content, RegDate, location };
+// }
+
+// createData(
+//   "운영자",
+//   "15일 배송건에 대해서 공지사항 드립니다.",
+
+//   "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//   "2022-03-15"
+// ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   ),
+//   createData(
+//     "운영자",
+//     "15일 배송건에 대해서 공지사항 드립니다.",
+//     "배송이 늦어져서 죄송합니다. 19일까지 배송 완료 될 예정입니다.",
+
+//     "2022-03-15"
+//   );
--- src/pages/Posttwo.js
+++ src/pages/Posttwo.js
@@ -4,20 +4,64 @@ import "./Post.css";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SendIcon from "@mui/icons-material/Send";
 import Stack from "@mui/material/Stack";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { ConsoleSqlOutlined } from "@ant-design/icons";
+import Table from 'react-bootstrap/Table'
 
 function Post(props) {
+
+  const [ detail, setDetail ] = useState([]);
+  const location = useLocation();
+  const id_data = location.id_value;
+
+  useEffect(() => {
+    console.log(id_data);
+    getNoticeDetail(id_data);
+  }, []);
+  
+  const getNoticeDetail = async (idx) => {
+    await axios
+      .get(
+        "http://192.168.0.119:8080/MainView/noticeDetail",
+        {
+          params: { notice_Idx: idx },
+        }
+      )
+      .then((res) => {
+        console.log("가져온값 : " + res.data.length);
+        console.log("가져온값 : " + JSON.stringify(res.data));
+        setDetail(res.data);
+      })
+      .catch((error) => {
+        console.log(`getNoticeDetail 에러 :  ${error.message}`);
+      });
+    
+  };
+
   return (
     <>
       <div className="Post_container">
-        <div className="blue_button">
-          <Button variant="contained" size="large">
-            15일 배송건에 대해서 공지사항 드립니다. 배송이 늦어져서 죄송합니다.
-            19일까지 배송 완료 될 예정입니다
-          </Button>
+        <div className="notice_detail">
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>{detail.back_Notice_Idx}</th>
+                <th colSpan={3}> 제목 : {detail.back_Notice_Subject}</th>
+                <th>{detail.back_Notice_RegDate}</th>
+              </tr>
+            </thead>
+            <tr>
+              <td colSpan={5}>
+                {detail.back_Notice_Content}
+              </td>
+            </tr>
+          </Table>
           <br />
           <br />
-
-          <Button href="Write">수정</Button>
+          <Button href="Write" >수정</Button>
           <Button href="Notice">목록</Button>
         </div>
       </div>
